ac7b2beea6214d10ddcab5eafb022e7191a0a71b
Nadiia <zoryvchaknv@gmail.com>
2024-12-02 12:25:12 +0200

    make build

---
+++ index.260620d3.js
@@ -0,0 +1,2 @@
+document.querySelector("table").querySelectorAll("tr").forEach(function(e){var r=e.children[1].cloneNode(!0);e.insertBefore(r,e.children[e.children.length-1])});
+//# sourceMappingURL=index.260620d3.js.map
